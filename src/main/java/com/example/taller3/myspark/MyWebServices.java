49559726b237d2636e4b5980e380061af152d155
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2024-02-09 20:27:23 -0500

    Ajustes implementación de ejemplo

--- src/main/java/com/example/taller3/myspark/MyWebServices.java
+++ src/main/java/com/example/taller3/myspark/MyWebServices.java
@@ -5,31 +5,23 @@ import java.net.URISyntaxException;
 
 //
 public class MyWebServices {
+    private static String headers = "HTTP/1.1 200 OK\r\n"
+            + "Content-Type:text/html\r\n"
+            + "\r\n";
+
     public static void main(String[] args) throws IOException, URISyntaxException {
-        HttpServer.get("/arep", (p) -> {
-            String resp = "HTTP/1.1 200 OK\r\n"
-                    + "Content-Type:text/html\r\n"
-                    + "\r\n"
-                    + "<h1>Hello AREP" + p + "!</h1>";
-            return resp;
+        HttpServer.get("/arep", p -> headers + "<h1>Hello AREP" + p + "!</h1>");
+
+        HttpServer.get("/arsw", p -> headers + "<h1>Hello to Vietnam Motherfoka!</h1>");
+
+        HttpServer.get("/ieti", p -> headers + "<h1>Hello IETI °.°</h1>");
+
+        HttpServer.get("/queries", param -> {
+            String title = "<title> Parameter tester </title>\r\n";
+            String content = "<p>Test " + param + "</p>";
+            return headers + title + content;
         });
 
-        HttpServer.get("/arsw", (p) -> {
-            String resp = "HTTP/1.1 200 OK\r\n"
-                    + "Content-Type:text/html\r\n"
-                    + "\r\n"
-                    + "<h1>Hello to Vietnam Motherfoka!</h1>";
-            return resp;
-        });
-
-        HttpServer.get("/ieti", (p) -> {
-            String resp = "HTTP/1.1 200 OK\r\n"
-                    + "Content-Type:text/html\r\n"
-                    + "\r\n"
-                    + "<h1>Hello IETI °.°</h1>";
-            return resp;
-        });
-
-        HttpServer.getInstance().runServer(null);
+        HttpServer.getInstance().runServer("prueba");
     }
 }
