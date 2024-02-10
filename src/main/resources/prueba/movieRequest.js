bc2b753c6c76b0b9bd1e0f4cf047db5f23c3e0f2
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2024-02-10 12:53:17 -0500

    Cambios JS del usuario y nueva imagen

--- src/main/resources/prueba/movieRequest.js
+++ src/main/resources/prueba/movieRequest.js
@@ -2,7 +2,7 @@ function getMovieData() {
   let nameVar = document.getElementById("title").value;
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function () {
-    document.getElementById("movieData").innerHTML = JSON.stringify(JSON.parse(this.responseText), null, <br></br>);
+    document.getElementById("movieData").innerHTML = this.responseText;
   };
   xhttp.open("GET", "/movie?movie=" + nameVar);
   xhttp.send();
